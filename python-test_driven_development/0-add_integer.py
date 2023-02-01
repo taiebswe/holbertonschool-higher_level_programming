718c95d31945e91272a1c99d1505f9c357d474e5
Your Name <you@example.com>
2023-02-01 13:49:04 +0000

    r

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    if not(isinstance(a, (float, int))):
+        raise("a must be an integer")
+    if not(isinstance(b, (float, int))):
+        raise("b must be an integer")
     return (int(a) + int(b))
