52594ef158525f88fa24124dbdb0a9daa7ce43b3
Your Name <you@example.com>
2023-02-02 09:50:24 +0000

    T

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,23 @@
+
 #!/usr/bin/python3
-""" Doc """
-
 def matrix_divided(matrix, div):
-    """ Doc """
-    if div == float('inf') or div == -float('inf') or div != div:
-        div = 10
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
-        raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for c in row:
-            if type(c) is not int and type(c) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
+    """ divides all elements of a matrix """
+    error = ("matrix must be a matrix (list of lists) of integers/floats")
+    ref_size = len(matrix[0])
+    matrix_matrix = []
+    for i in matrix:
+        valeur_matrix = []
+        for j in i:
+            if type(div) not in [int, float]:
+                raise TypeError("div must be a number")
+            elif type(j) not in [int, float]:
+                raise TypeError(error)
 
-    return [[round(item / div, 2) for item in row] for row in matrix]
+            if (div == float("inf")):
+                valeur_matrix.append(0.0)
+            else:
+                valeur_matrix.append(round(float(j / div), 2))
+        if ref_size != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        matrix_matrix.append(valeur_matrix)
+    return matrix_matrix
